0d9318ca1e97684a4c11261446bbaf38d4c5aa29
Alejandra Madrid <alejandra.madrid1426@gmail.com>
2026-08-31 08:50:59 -0500

    feat: add members API endpoint

---
+++ TeamGitPractice/Controllers/MembersController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamGitPractice.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MembersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetMembers()
+    {
+        var members = new[]
+        {
+            "Juan David Velásquez Murillo",
+            "Alejandra Madrid Calderón",
+            "Sara Regino Ferraro",
+            "José David Vásquez Díaz",
+            "Paula Andrea Calderón Quintero"
+        };
+
+        return Ok(members);
+    }
+}
